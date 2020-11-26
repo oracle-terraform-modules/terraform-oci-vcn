16700227929e60495332537c55937abb03ee5ece
Ali Mukadam <ali.mukadam@oracle.com>
2020-11-27 08:26:37 +1100

    set minimum version to Terraform 0.13

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
 # Copyright (c) 2019, 2020 Oracle Corporation and/or affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
-terraform {
-  required_version = ">= 0.12.24"
-}
