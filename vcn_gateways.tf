4788ff568c5886d2385af279d472bb4f81e7c623
Çetin ARDAL <6201562+kral2@users.noreply.github.com>
2021-02-08 23:45:19 +0100

    Update contributing guide (#25)
    * update CONTRIBUTING guide
    
    add a coding convention document
    add new sections on CONTRIBUTING.adoc with focus on documentation update
    fix some headers on CONTRIBUTORS.adoc
    
    * update CHANGELOG
    
    * Add requested changes during PR review
    
    fix typo on resource oci_core_internet_gateway.ig
    
    Co-authored-by: Avi Miller <avi.miller@oracle.com>
    
    Co-authored-by: Çetin ARDAL <kral2@users.noreply.github.com>
    Co-authored-by: Avi Miller <avi.miller@oracle.com>

--- vcn_gateways.tf
+++ vcn_gateways.tf
@@ -1,7 +1,7 @@
 # Copyright (c) 2019, 2021, Oracle Corporation and/or affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-resource oci_core_internet_gateway "ig" {
+resource "oci_core_internet_gateway" "ig" {
   compartment_id = var.compartment_id
   display_name   = var.label_prefix == "none" ? "internet-gateway" : "${var.label_prefix}-internet-gateway"
             
