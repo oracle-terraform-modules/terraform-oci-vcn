fbc24f79fa2fa1851922c8f265047245d37e3915
Çetin ARDAL <cetin.ardal@oracle.com>
2021-03-12 20:44:07 +0100

    fix issue #34 (add output for full parity of attributes with provider)
    - outputs use a for expression and will auto-update as provider adds new attributes
    - update legacy splat syntax toward the current standard
    
    Signed-off-by: Çetin ARDAL <cetin.ardal@oracle.com>

--- outputs.tf
+++ outputs.tf
@@ -8,25 +8,73 @@ output "vcn_id" {
 
 output "drg_id" {
   description = "id of drg if it is created"
-  value       = join(",", oci_core_drg.drg.*.id)
+  value       = join(",", oci_core_drg.drg[*].id)
 }
 
 output "nat_gateway_id" {
   description = "id of nat gateway if it is created"
-  value       = join(",", oci_core_nat_gateway.nat_gateway.*.id)
+  value       = join(",", oci_core_nat_gateway.nat_gateway[*].id)
 }
 
 output "internet_gateway_id" {
   description = "id of internet gateway if it is created"
-  value       = join(",", oci_core_internet_gateway.ig.*.id)
+  value       = join(",", oci_core_internet_gateway.ig[*].id)
+}
+
+output "service_gateway_id" {
+  description = "id of service gateway if it is created"
+  value       = join(",", oci_core_service_gateway.service_gateway[*].id)
 }
 
 output "ig_route_id" {
   description = "id of internet gateway route table"
-  value       = join(",", oci_core_route_table.ig.*.id)
+  value       = join(",", oci_core_route_table.ig[*].id)
 }
 
 output "nat_route_id" {
   description = "id of VCN NAT gateway route table"
-  value       = join(",", oci_core_route_table.nat.*.id)
+  value       = join(",", oci_core_route_table.nat[*].id)
+}
+
+# New complete outputs for each resources with provider parity. Auto-updating.
+# Usefull for module composition.
+
+output "drg_all_attributes" {
+  description = "all attributes of created drg"
+  value       = { for k, v in oci_core_drg.drg : k => v }
+}
+
+output "drg_attachment_all_attributes" {
+  description = "all attributes related to drg attachment"
+  value       = { for k, v in oci_core_drg_attachment.drg : k => v }
+}
+
+output "internet_gateway_all_attributes" {
+  description = "all attributes of created internet gateway"
+  value       = { for k, v in oci_core_internet_gateway.ig : k => v }
+}
+
+output "ig_route_all_attributes" {
+  description = "all attributes of created ig route table"
+  value       = { for k, v in oci_core_route_table.ig : k => v }
+}
+
+output "nat_gateway_all_attributes" {
+  description = "all attributes of created nat gateway"
+  value       = { for k, v in oci_core_nat_gateway.nat_gateway : k => v }
+}
+
+output "nat_route_all_attributes" {
+  description = "all attributes of created nat gateway route table"
+  value       = { for k, v in oci_core_route_table.nat : k => v }
+}
+
+output "service_gateway_all_attributes" {
+  description = "all attributes of created service gateway"
+  value       = { for k, v in oci_core_service_gateway.service_gateway : k => v }
+}
+
+output "vcn_all_attributes" {
+  description = "all attributes of created vcn"
+  value       = { for k, v in oci_core_vcn.vcn : k => v }
 }
