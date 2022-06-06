0208a8dbeee03581edc6cba04218c5b8d1c3be76
Karthic Ravindran <karthic.ravindran@oracle.com>
2022-06-06 18:13:49 +0530

    docs: Updated for subnet

--- modules/subnet/subnet.tf
+++ modules/subnet/subnet.tf
@@ -1,4 +1,4 @@
-# Copyright (c) 2021, 2022 Oracle Corporation and/or affiliates.  All rights reserved.
+# Copyright (c) 2022 Oracle Corporation and/or affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 locals {
