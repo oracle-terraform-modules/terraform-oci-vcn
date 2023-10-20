oracle-terraform-modules/terraform-oci-vcn

commit 4d8ff34e044eb2deddbf3780547862010e609b54

feat: Support disabled DNS resolution with optional dns_label on VCN (#101)
* Support disabled DNS resolution with optional dns_label on VCN

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

* Change vcn_name default from "vcn-module" to "vcn"

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

Signed-off-by: Devon Crouse <devon.crouse@oracle.com>

variables.tf
@@ -1,4 +1,4 @@
-# Copyright (c) 2019, 2021 Oracle Corporation and/or affiliates.  All rights reserved.
+# Copyright (c) 2019, 2022 Oracle Corporation and/or affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
 # provider identity parameters
@@ -83,20 +83,20 @@ variable "vcn_cidrs" {
 }
 
 variable "vcn_dns_label" {
-  description = "A DNS label for the VCN, used in conjunction with the VNIC's hostname and subnet's DNS label to form a fully qualified domain name (FQDN) for each VNIC within this subnet"
+  description = "A DNS label for the VCN, used in conjunction with the VNIC's hostname and subnet's DNS label to form a fully qualified domain name (FQDN) for each VNIC within this subnet. DNS resolution of hostnames in the VCN is disabled when null."
   type        = string
   default     = "vcnmodule"
 
   validation {
-    condition     = length(regexall("^[^0-9][a-zA-Z0-9_]+$", var.vcn_dns_label)) > 0
-    error_message = "DNS label must be an alphanumeric string that begins with a letter."
+    condition     = var.vcn_dns_label == null ? true : length(regexall("^[^0-9][a-zA-Z0-9_]{1,14}$", var.vcn_dns_label)) > 0
+    error_message = "DNS label must be unset to disable, or an alphanumeric string with length of 1 through 15 that begins with a letter."
   }
 }
 
 variable "vcn_name" {
   description = "user-friendly name of to use for the vcn to be appended to the label_prefix"
   type        = string
-  default     = "vcn-module"
+  default     = "vcn"
   validation {
     condition     = length(var.vcn_name) > 0
     error_message = "The vcn_name value cannot be an empty string."

examples/custom_route_rules/variables.tf
@@ -110,7 +110,7 @@ variable "vcn_dns_label" {
 variable "vcn_name" {
   description = "user-friendly name of to use for the vcn to be appended to the label_prefix"
   type        = string
-  default     = "vcn-module"
+  default     = "vcn"
 }
 
 # gateways parameters

examples/hub-spoke/variables.tf
@@ -109,7 +109,7 @@ variable "vcn_dns_label" {
 variable "vcn_name" {
   description = "user-friendly name of to use for the vcn to be appended to the label_prefix"
   type        = string
-  default     = "vcn-module"
+  default     = "vcn"
 }
 
 # gateways parameters

examples/module_composition/variables.tf
@@ -109,7 +109,7 @@ variable "vcn_dns_label" {
 variable "vcn_name" {
   description = "user-friendly name of to use for the vcn to be appended to the label_prefix"
   type        = string
-  default     = "vcn-module"
+  default     = "vcn"
 }
 
 # gateways parameters

modules/subnet/subnet.tf
@@ -26,7 +26,7 @@ resource "oci_core_subnet" "vcn_subnet" {
   security_list_ids          = null
 
   lifecycle {
-    ignore_changes = [defined_tags, freeform_tags]
+    ignore_changes = [defined_tags, dns_label, freeform_tags]
   }
 }
 

schema.yaml
@@ -46,7 +46,7 @@ variables:
   vcn_dns_label:
     title: VCN DNS Label
     type: string
-    required: true
+    required: false
     default: vcn
 
   lockdown_default_seclist:

vcn.tf
@@ -1,4 +1,4 @@
-# Copyright (c) 2019, 2021, Oracle Corporation and/or affiliates.
+# Copyright (c) 2019, 2022 Oracle Corporation and/or affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
 resource "oci_core_vcn" "vcn" {
@@ -14,7 +14,7 @@ resource "oci_core_vcn" "vcn" {
   defined_tags  = var.defined_tags
 
   lifecycle {
-    ignore_changes = [defined_tags, freeform_tags]
+    ignore_changes = [defined_tags, dns_label, freeform_tags]
   }
 }
 

commit a5ece1604ad810570313b2de491808035aff8b02

fix: for breaking change introduced in #101 and causes destruction
and recreation of existing infrastructure

Signed-off-by: Ali Mukadam <ali.mukadam@oracle.com>

outputs.tf
@@ -29,7 +29,7 @@ output "ig_route_id" {
 
 output "nat_route_id" {
   description = "id of VCN NAT gateway route table"
-  value       = join(",", oci_core_route_table.nat[*].id)
+  value       = var.update_nat_route_table ? join(",", oci_core_route_table.nat[*].id) : join(",", oci_core_route_table.nat_table_changes_ignored[*].id)
 }
 
 output "sgw_route_id" {

variables.tf
@@ -157,8 +157,8 @@ variable "nat_gateway_route_rules" {
 
 variable "attached_drg_id" {
   description = "the ID of DRG attached to the VCN"
-  type = string
-  default = null
+  type        = string
+  default     = null
 }
 
 #subnets
@@ -166,4 +166,11 @@ variable "subnets" {
   description = "Private or Public subnets in a VCN"
   type        = any
   default     = {}
+}
+
+# update route rule changes when running terraform apply again
+variable "update_nat_route_table" {
+  type        = bool
+  default     = true
+  description = "Update changes to NAT route rules on existing VCN and infra"
 }

vcn_defaultresources.tf
@@ -62,6 +62,6 @@ resource "oci_core_default_security_list" "restore_default" {
   lifecycle {
     ignore_changes = [egress_security_rules, ingress_security_rules, defined_tags]
   }
-  
+
   count = var.lockdown_default_seclist == false ? 1 : 0
 }

vcn_gateways.tf
@@ -10,7 +10,7 @@ resource "oci_core_internet_gateway" "ig" {
   display_name   = var.label_prefix == "none" ? var.internet_gateway_display_name : "${var.label_prefix}-${var.internet_gateway_display_name}"
 
   freeform_tags = var.freeform_tags
-  defined_tags = var.defined_tags
+  defined_tags  = var.defined_tags
 
   vcn_id = oci_core_vcn.vcn.id
 
@@ -26,7 +26,7 @@ resource "oci_core_route_table" "ig" {
   display_name   = var.label_prefix == "none" ? "internet-route" : "${var.label_prefix}-internet-route"
 
   freeform_tags = var.freeform_tags
-  defined_tags = var.defined_tags
+  defined_tags  = var.defined_tags
 
   route_rules {
     # * With this route table, Internet Gateway is always declared as the default gateway
@@ -50,12 +50,12 @@ resource "oci_core_route_table" "ig" {
     # * filter var.internet_gateway_route_rules for routes with "drg" as destination
     # * and steer traffic to the attached DRG if available
     for_each = var.internet_gateway_route_rules != null ? { for k, v in var.internet_gateway_route_rules : k => v
-    if v.network_entity_id == "drg" && var.attached_drg_id != null} : {}
+    if v.network_entity_id == "drg" && var.attached_drg_id != null } : {}
 
     content {
       destination       = route_rules.value.destination
       destination_type  = route_rules.value.destination_type
-      network_entity_id =  var.attached_drg_id
+      network_entity_id = var.attached_drg_id
       description       = route_rules.value.description
     }
   }
@@ -115,7 +115,7 @@ resource "oci_core_service_gateway" "service_gateway" {
   display_name   = var.label_prefix == "none" ? var.service_gateway_display_name : "${var.label_prefix}-${var.service_gateway_display_name}"
 
   freeform_tags = var.freeform_tags
-  defined_tags = var.defined_tags
+  defined_tags  = var.defined_tags
   services {
     service_id = lookup(data.oci_core_services.all_oci_services[0].services[0], "id")
   }
@@ -134,7 +134,7 @@ resource "oci_core_route_table" "service_gw" {
   display_name   = var.label_prefix == "none" ? "service-gw-route" : "${var.label_prefix}-service-gw-route"
 
   freeform_tags = var.freeform_tags
-  defined_tags = var.defined_tags
+  defined_tags  = var.defined_tags
 
   dynamic "route_rules" {
     # * If Service Gateway is created with the module, automatically creates a rule to handle traffic for "all services" through Service Gateway
@@ -165,7 +165,7 @@ resource "oci_core_nat_gateway" "nat_gateway" {
   display_name   = var.label_prefix == "none" ? var.nat_gateway_display_name : "${var.label_prefix}-${var.nat_gateway_display_name}"
 
   freeform_tags = var.freeform_tags
-  defined_tags = var.defined_tags
+  defined_tags  = var.defined_tags
 
   public_ip_id = var.nat_gateway_public_ip_id != "none" ? var.nat_gateway_public_ip_id : null
 
@@ -183,7 +183,7 @@ resource "oci_core_route_table" "nat" {
   display_name   = var.label_prefix == "none" ? "nat-route" : "${var.label_prefix}-nat-route"
 
   freeform_tags = var.freeform_tags
-  defined_tags = var.defined_tags
+  defined_tags  = var.defined_tags
 
   route_rules {
     # * With this route table, NAT Gateway is always declared as the default gateway
@@ -197,7 +197,7 @@ resource "oci_core_route_table" "nat" {
     # * filter var.nat_gateway_route_rules for routes with "drg" as destination
     # * and steer traffic to the attached DRG if available
     for_each = var.nat_gateway_route_rules != null ? { for k, v in var.nat_gateway_route_rules : k => v
-    if v.network_entity_id == "drg" && var.attached_drg_id != null} : {}
+    if v.network_entity_id == "drg" && var.attached_drg_id != null } : {}
 
     content {
       destination       = route_rules.value.destination
@@ -242,7 +242,7 @@ resource "oci_core_route_table" "nat" {
     ignore_changes = [defined_tags, freeform_tags]
   }
 
-  count = var.create_nat_gateway == true ? 1 : 0
+  count = var.create_nat_gateway && var.update_nat_route_table ? 1 : 0
 }
 
 
@@ -257,7 +257,7 @@ resource "oci_core_local_peering_gateway" "lpg" {
   display_name   = var.label_prefix == "none" ? each.key : "${var.label_prefix}-${each.key}"
 
   freeform_tags = var.freeform_tags
-  defined_tags = var.defined_tags
+  defined_tags  = var.defined_tags
 
   vcn_id = oci_core_vcn.vcn.id
 

vcn_nat_fix.tf
@@ -1,0 +1,83 @@
+# Copyright (c) 2019, 2021, Oracle Corporation and/or affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+# special fix due to bug introduced in #101 which causes destruction and recreation of subnets
+resource "oci_core_route_table" "nat_table_changes_ignored" {
+  compartment_id = var.compartment_id
+  display_name   = var.label_prefix == "none" ? "nat-route" : "${var.label_prefix}-nat-route"
+
+  freeform_tags = var.freeform_tags
+  defined_tags  = var.defined_tags
+
+  route_rules {
+    # * With this route table, NAT Gateway is always declared as the default gateway
+    destination       = local.anywhere
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_nat_gateway.nat_gateway[0].id
+    description       = "Terraformed - Auto-generated at NAT Gateway creation: NAT Gateway as default gateway"
+  }
+
+  # bring this block back to fix #101
+  dynamic "route_rules" {
+    # * If Service Gateway is created with the module, automatically creates a rule to handle traffic for "all services" through Service Gateway
+    for_each = var.create_service_gateway == true ? [1] : []
+
+    content {
+      destination       = lookup(data.oci_core_services.all_oci_services[0].services[0], "cidr_block")
+      destination_type  = "SERVICE_CIDR_BLOCK"
+      network_entity_id = oci_core_service_gateway.service_gateway[0].id
+      description       = "Terraformed - Auto-generated at Service Gateway creation: All Services in region to Service Gateway"
+    }
+  }
+
+  dynamic "route_rules" {
+    # * filter var.nat_gateway_route_rules for routes with "drg" as destination
+    # * and steer traffic to the attached DRG if available
+    for_each = var.nat_gateway_route_rules != null ? { for k, v in var.nat_gateway_route_rules : k => v
+    if v.network_entity_id == "drg" && var.attached_drg_id != null } : {}
+
+    content {
+      destination       = route_rules.value.destination
+      destination_type  = route_rules.value.destination_type
+      network_entity_id = var.attached_drg_id
+      description       = route_rules.value.description
+    }
+  }
+
+  dynamic "route_rules" {
+    # * filter var.nat_gateway_route_rules for routes with "nat_gateway" as destination
+    # * and steer traffic to the module created NAT Gateway
+    for_each = var.nat_gateway_route_rules != null ? { for k, v in var.nat_gateway_route_rules : k => v
+    if v.network_entity_id == "nat_gateway" } : {}
+
+    content {
+      destination       = route_rules.value.destination
+      destination_type  = route_rules.value.destination_type
+      network_entity_id = oci_core_nat_gateway.nat_gateway[0].id
+      description       = route_rules.value.description
+    }
+  }
+
+  dynamic "route_rules" {
+    # * filter var.nat_gateway_route_rules for generic routes
+    # * can take any Named Value : String, Input Variable, Local Value, Data Source, Resource, Module Output ...
+    # * useful for gateways that are not managed by the module
+    for_each = var.nat_gateway_route_rules != null ? { for k, v in var.nat_gateway_route_rules : k => v
+    if contains(["drg", "nat_gateway"], v.network_entity_id) == false } : {}
+
+    content {
+      destination       = route_rules.value.destination
+      destination_type  = route_rules.value.destination_type
+      network_entity_id = route_rules.value.network_entity_id
+      description       = route_rules.value.description
+    }
+  }
+
+  vcn_id = oci_core_vcn.vcn.id
+
+  lifecycle {
+    ignore_changes = [defined_tags, freeform_tags, route_rules]
+  }
+
+  count = var.create_nat_gateway && !var.update_nat_route_table ? 1 : 0
+}