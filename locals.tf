95ae16ad0434bf9bcdb474ed1639cf796b2d3243
syedthameem85 <150205478+syedthameem85@users.noreply.github.com>
2024-02-19 05:59:36 +0000

    feat: Full naming support for route tables rather than just the prefix (#137)
    Resolves #137 103

--- locals.tf
+++ locals.tf
@@ -4,4 +4,22 @@
 locals {
   anywhere      = "0.0.0.0/0"
   anywhere_ipv6 = "::/0"
+  internet_gateway_display_name = coalesce(
+    var.label_prefix == "none" && var.internet_gateway_display_name == "none" ? "internet-route" : null,
+    var.label_prefix == "none" && var.internet_gateway_display_name != "none" ? var.internet_gateway_display_name : null,
+    var.label_prefix != "none" && var.internet_gateway_display_name == "none" ? "${var.label_prefix}-internet-route" : null,
+    var.label_prefix != "none" && var.internet_gateway_display_name != "none" ? "${var.label_prefix}-${var.internet_gateway_display_name}" : null
+  )
+  service_gateway_display_name = coalesce(
+    var.label_prefix == "none" && var.service_gateway_display_name == "none" ? "service-gw-route" : null,
+    var.label_prefix == "none" && var.service_gateway_display_name != "none" ? var.service_gateway_display_name : null,
+    var.label_prefix != "none" && var.service_gateway_display_name == "none" ? "${var.label_prefix}-service-gw-route" : null,
+    var.label_prefix != "none" && var.service_gateway_display_name != "none" ? "${var.label_prefix}-${var.service_gateway_display_name}" : null
+  )
+  nat_gateway_display_name = coalesce(
+    var.label_prefix == "none" && var.nat_gateway_display_name == "none" ? "nat-route" : null,
+    var.label_prefix == "none" && var.nat_gateway_display_name != "none" ? var.nat_gateway_display_name : null,
+    var.label_prefix != "none" && var.nat_gateway_display_name == "none" ? "${var.label_prefix}-nat-route" : null,
+    var.label_prefix != "none" && var.nat_gateway_display_name != "none" ? "${var.label_prefix}-${var.nat_gateway_display_name}" : null
+  )
 }
